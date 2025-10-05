521bcd93322564ad8d798306ccf82f0821b73189
Hoang Thien <hohoangthien.hht@gmail.com>
2025-10-05 14:42:22 +0000

    Revert Version + add logger

--- version_checker.py
+++
@@ -1,9 +0,0 @@
-# version_checker.py
-from pyspark.sql import SparkSession
-
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("SparkVersionChecker").getOrCreate()
-    print("="*60)
-    print(f"SPARK VERSION ON CLUSTER: {spark.version}")
-    print("="*60)
-    spark.stop()
